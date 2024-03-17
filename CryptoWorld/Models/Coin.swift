3d4b3e3e6cced87856ea469a0b068e1631f6373c
Ivan Semikin <i.semikin.94@icloud.com>
2024-03-17 15:44:16 +0100

    Create model Coin

--- CryptoWorld/Models/Coin.swift
+++ CryptoWorld/Models/Coin.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Coin: Decodable {
+    let name: String
+    let symbol: String
+    let image: URL
+    let current_price: Double
+    let high_24h: Double
+    let low_24h: Double
+    let price_change_percentage_24h: Double
+    let last_updated: String
+}
