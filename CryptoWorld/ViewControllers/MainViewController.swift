c18b3724c9215ec90b43000d3b2d39df455162a6
Ivan Semikin <i.semikin.94@icloud.com>
2024-03-17 16:03:21 +0100

    Update MainVC with parse JSON

--- CryptoWorld/ViewControllers/MainViewController.swift
+++ CryptoWorld/ViewControllers/MainViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
-    private let link = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10")
+    private let link = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchCoins()
     }
 
-
+    private func fetchCoins() {
+        URLSession.shared.dataTask(with: link) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let coins = try JSONDecoder().decode([Coin].self, from: data)
+                
+                coins.forEach { coin in
+                    print("\(coin)\n")
+                }
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+    }
 }
 
